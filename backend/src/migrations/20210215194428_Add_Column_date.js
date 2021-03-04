980ff06a914e8ed7fa4fdf0e425504c7326d5241
Guilherme Schubert Medeiros <guilhermeschu10@gmail.com>
2021-03-03 23:02:48 -0300

    Alterado configurações de insert de data no DB

--- backend/src/migrations/20210215194428_Add_Column_date.js
+++ backend/src/migrations/20210215194428_Add_Column_date.js
@@ -1,12 +1,13 @@
+const tempo = new Date().toLocaleString()
 
 exports.up = function(knex) {
     return knex.schema.table('infoComissao', table => {
-        table.date('created_at').defaultTo(knex.fn.now())
+        table.date('created_at').defaultTo(tempo)
     })  
 };
 
 exports.down = function(knex) {
     return knex.schema.table('infoComissao'), table => {
-        table.dropColumn('createdAt')
+        table.dropColumn('created_at')
     }
 };
